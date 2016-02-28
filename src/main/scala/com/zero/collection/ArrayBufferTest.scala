c10c30a42b8396e48e4efb74a2059d92ba7605dd
jianjia1 <jianjia1@staff.sina.com.cn>
2016-02-28 16:52:14 +0800

    ArrayBuffer Test

--- src/main/scala/com/zero/collection/ArrayBufferTest.scala
+++ src/main/scala/com/zero/collection/ArrayBufferTest.scala
@@ -7,37 +7,41 @@ import scala.collection.mutable.ArrayBuffer
   */
 object ArrayBufferTest {
     def main(args: Array[String]) {
-        val b = ArrayBuffer[Int]()
-        b += 1
-        b +=(1, 2, 3, 5)
-        b ++= Array(8, 13, 21)
-        b.trimEnd(5)
-        b.insert(2, 6)
-        b.insert(2, 7, 8, 9)
-        b.remove(2)
-        b.remove(2, 3)
+        val b = ArrayBuffer[Int]() //变长数组，或则 new ArrayBuffer[Int]
+        b += 1 //ArrayBuffer(1), += 是在尾端添加
+        b +=(1, 2, 3, 5) //ArrayBuffer(1, 1, 2, 3, 5)
+        b ++= Array(8, 13, 21) //++= 追加集合，ArrayBuffer(1, 1, 2, 3, 5, 8, 13, 21)
+        b.trimEnd(5) //移除最后5个元素，ArrayBuffer(1, 1, 2)
+        println(b) //ArrayBuffer(1, 1, 2)
+
+        b.insert(2, 6) //在下标2位置插入 6
+        b.insert(2, 7, 8, 9) //在下标2位置插入 7, 8, 9
+        println(b) //ArrayBuffer(1, 1, 7, 8, 9, 6, 2)
+
+        b.remove(2) //移除下标2位置的元素
+        println(b) //ArrayBuffer(1, 1, 8, 9, 6, 2)
+        b.remove(2, 3) //从下标2位置的元素开始移除3个元素
+        println(b) //ArrayBuffer(1, 1, 2)
+
         for (item <- b) {
-            print(item + " ")
+            //遍历，方式一
+            print(item + " ") //1 1 2
         }
-
+        for (i <- 0 until b.size) {
+            //遍历，方式二
+            print(b(i) + " ") //1 1 2
+        }
         println()
+
+        println(0 until(10, 2)) //Range(0, 2, 4, 6, 8)
+        println((0 until (5)).reverse) //Range(4, 3, 2, 1, 0)
+
         val c = Array(2, 3, 5, 7, 11)
-        val result = for (elem <- c) yield 2 * elem
-        for (item <- result) {
-            print(item + " ")
-        }
-        println()
-
         val result1 = c.map(2 * _)
         for (item <- result1) {
             print(item + " ")
         }
-
         println()
-        for (elem <- c if elem % 2 == 0) yield 2 * elem
-        c.filter(_ % 2 == 0).map(2 * _)
-        println(c)
-        println(c)
 
         Array(1, 7, 2, 9).sum
         ArrayBuffer("Mary", "had", "a", "little", "lamb").max
@@ -64,6 +68,5 @@ object ArrayBufferTest {
         for (i <- 0 until triangle.length)
             triangle(i) = new Array[Int](i + 1)
     }
-
 
 }
