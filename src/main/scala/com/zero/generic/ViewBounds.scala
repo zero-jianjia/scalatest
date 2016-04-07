a81381eec42c68ba0abf72eefe45fff4af4b0a6e
zero-jianjia <zero_jianjia@163.com>
2016-04-07 20:09:03 +0800

    update

---
+++ src/main/scala/com/zero/generic/ViewBounds.scala
@@ -0,0 +1,29 @@
+package com.zero.generic
+
+//class Pair_NotPerfect[T <: Comparable[T]](val first : T,val second : T){
+//  def bigger = if(first.compareTo(second) > 0)first else second  
+//}
+//<%，视图界定，将T类型的实例转换为Comparable[T]类型
+class PairNotPerfect[T <% Comparable[T]](val first: T, val second: T) {
+    def bigger = if (first.compareTo(second) > 0) first else second
+}
+
+class PairBetter[T <% Ordered[T]](val first: T, val second: T) {
+    def bigger = if (first > second) first else second
+}
+
+object ViewBounds {
+    def main(args: Array[String]) {
+        val pair = new PairNotPerfect("Spark", "Hadoop")
+        println(pair.bigger)
+
+        val pairInt = new PairNotPerfect(3, 5) //Int -> RichInt
+        println(pairInt.bigger)
+
+        val pair_Better_String = new PairBetter("Java", "Scala") //String -> RichString
+        println(pair_Better_String.bigger)
+
+        val pair_Better_Int = new PairBetter(20, 12)
+        println(pair_Better_Int.bigger)
+    }
+}
--- src/main/scala/com/zero/oop/TraitTest.scala
+++ src/main/scala/com/zero/oop/TraitTest.scala
@@ -1,4 +1,4 @@
-package com.zero.traittest
+package com.zero.oop
 
 class B {
     override def toString = "class B"
