4e4ea5e47bc2cc301b77728c9872c036bde4ba8a
zero-jianjia <zero_jianjia@163.com>
2016-05-19 21:45:52 +0800

    隐式函数

--- src/main/scala/com/zero/implicit/ImplicitConversion.scala
+++ src/main/scala/com/zero/implicit/ImplicitConversion.scala
@@ -8,12 +8,21 @@ class RichFile(val file: File) {
 }
 
 object Context {
-    implicit def file2RichFile(file: File) = new RichFile(file) //File -> RichFile
+    implicit def file2RichFile(file: File) = new RichFile(file)
+    
+    implicit def int2Fraction(n: Int) = new Fraction(1, n)
+}
+
+class Fraction(val x: Int, val y: Int) {
+    def add(other: Fraction) = {
+        1
+    }
 }
 
 object ImplicitConversion {
     def main(args: Array[String]) {
-        import Context.file2RichFile
+        import Context._
         println(new File("build.gradle").read)
+        val result = 7.add(new Fraction(1, 2))
     }
 }
