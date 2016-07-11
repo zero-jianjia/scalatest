fe958be3f1f891e98c4bb542daa1f675e5e07e7a
jianjia1 <jianjia1@staff.sina.com.cn>
2016-07-11 18:01:11 +0800

    update

--- src/main/scala/org/zero/twitter/HashedWheelTimerTest.scala
+++ src/main/scala/org/zero/twitter/HashedWheelTimerTest.scala
@@ -2,10 +2,11 @@ package org.zero.twitter
 
 import java.time.LocalDateTime
 import java.util.TimeZone
+import java.util.concurrent.TimeUnit
 
 import com.twitter.conversions.time._
 import com.twitter.finagle.util.HashedWheelTimer
-import com.twitter.util.{Duration, Time, TimeFormat}
+import com.twitter.util.{Duration, ScheduledThreadPoolTimer, Time, TimeFormat}
 import org.zero.DateUtil
 
 /**
@@ -19,20 +20,29 @@ object HashedWheelTimerTest {
         val now = timeFormat.parse("201606241205")
         val period = 10
         var expertTime = timeFormat.format(now.plus(period.minutes)).toLong
-//        val diff = if (expertTime % period == 0) -period else expertTime % period
+        //        val diff = if (expertTime % period == 0) -period else expertTime % period
         expertTime = expertTime - expertTime % period
         println(expertTime)
 
         //        val a = timeFormat.format(expertTime)
         //        println(a)
+        test2
     }
 
     def test2: Unit = {
         println(System.currentTimeMillis())
-        HashedWheelTimer.apply(Duration.fromSeconds(1), 5).schedule(Time.fromSeconds(5), 5.seconds)({
-            println(System.currentTimeMillis() + " run")
-        })
+        //new ScheduledThreadPoolTimer().
+        //HashedWheelTimer.apply(Duration.fromSeconds(1), 5)
+        val timer =  new ScheduledThreadPoolTimer()
 
+        timer.schedule(Time.fromSeconds(5), 5.seconds)({
+            println(System.currentTimeMillis() / 1000 + " a run")
+            TimeUnit.SECONDS.sleep(3)
+        })
+        timer.schedule(Time.fromSeconds(2), 3.seconds)({
+            println(System.currentTimeMillis() / 1000 + " b run")
+            TimeUnit.SECONDS.sleep(2)
+        })
         Thread.sleep(1000000)
     }
 
